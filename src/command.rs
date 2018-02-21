bf6e07888c9275c47f4e98c062821b2dece7c696
James Forcier <csssuf@csssuf.net>
2018-02-21 02:58:42 -0500

    command: actually send last.fm track

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,7 @@
 use rocket::State;
 use rocket::request::LenientForm;
 
+use lastfm::*;
 use slack::*;
 
 #[derive(FromForm)]
@@ -20,11 +21,17 @@ struct CommandRequest {
 #[post("/np", data = "<payload>")]
 fn command_np(
     slack_client: State<SlackClient>,
+    lastfm_client: State<LastfmClient>,
     payload: LenientForm<CommandRequest>,
-) -> Result<String, String> {
+) -> Result<(), String> {
     let payload = payload.get();
 
-    let lastfm_username = slack_client.get_custom_field("LastFM", &payload.user_id)?;
-
-    Ok(format!("Your Last.FM username is: {:?}", lastfm_username))
+    if let Some(lastfm_username) = slack_client.get_custom_field("LastFM", &payload.user_id)? {
+        let now_playing = lastfm_client.now_playing(&lastfm_username)?;
+        let message = format!("{} is now playing: {} - {}", lastfm_username, now_playing.artist, now_playing.name);
+        slack_client.post_message(&payload.channel_id, &message)?;
+        Ok(())
+    } else {
+        Err(format!("Sorry, you don't have a Last.FM username set in your slack profile."))
+    }
 }
